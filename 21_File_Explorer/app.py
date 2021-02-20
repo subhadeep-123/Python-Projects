a4a6768caa6ddac54e7faebdc633413b6742380d
Subhadeep Banerjee <subhadeep@klizos.com>
2021-02-21 03:44:32 +0530

    Add Various Features

--- 21_File_Explorer/app.py
+++ 21_File_Explorer/app.py
@@ -1,9 +1,11 @@
 import os
 import sys
+import shutil
 import getpass
 import logging
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
 from PIL import Image, ImageTk
 
 # Local Imports
@@ -11,7 +13,7 @@ import errors
 
 root = Tk()
 root.iconbitmap('assets/icon.ico')
-root.geometry("544x500")
+root.geometry("544x400")
 root.title('File Explorer')
 
 logger = logging.getLogger(__name__)
@@ -33,13 +35,54 @@ if sys.platform.startswith('linux'):
     initialdirname = f"/home/{getpass.getuser()}/Desktop"
 
 
+def popup(data=None, mode=None) -> None:
+    global write_data, newname, window, newdirname
+    window = Toplevel()
+    if mode == 'List':
+        label0 = Label(
+            window, text=f"Selected Directory is:\n{dirname}\n------------------------------").pack()
+        for i in data:
+            label = Label(window, text=i).pack()
+    if mode == 'Read':
+        label0 = Label(
+            window, text=f"Selected File is:\n{data[0]}\n------------------------------").pack()
+        with open(data[0], 'r') as f:
+            label = Label(window, text=f.read()).pack()
+    if mode == 'Write':
+        label0 = Label(
+            window, text=f"Selected File is:\n{data[0]}\n------------------------------").pack()
+        write_data = Entry(window, width=30)
+        write_data.pack()
+        btn = Button(window, text='WriteToFile',
+                     command=write_to_file).pack()
+    if mode == 'RenameFile':
+        label0 = Label(
+            window, text=f"Selected File is:\n{data[0]}\n------------------------------").pack()
+        newname = Entry(window, width=30)
+        newname.pack()
+        btn = Button(window, text='Save New Name',
+                     command=file_rename).pack()
+    if mode == 'RenameDir':
+        label0 = Label(
+            window, text=f"Selected Directory is:\n{data[0]}\n------------------------------").pack()
+        newdirname = Entry(window, width=30)
+        newdirname.pack()
+        btn = Button(window, text='Save New Name',
+                     command=dir_rename).pack()
+    if mode == 'MakeDir':
+        label0 = Label(
+            window, text=f"Selected Directory is:\n{data[0]}\n------------------------------").pack()
+        newdirname = Entry(window, width=30)
+        newdirname.pack()
+        btn = Button(window, text='Save Dir Name',
+                     command=make_dir).pack()
+    btn = Button(window, text='Close', command=window.destroy).pack()
+
+
 def OpenDirectory():
-    global dirname, in_dir_contents
+    global dirname
     dirname = filedialog.askdirectory(title='Select and Directory')
     logger.info(f"Selected Directory - {os.path.split(dirname)[1]}")
-    in_dir_contents = os.listdir(dirname)
-    for i in in_dir_contents:
-        label = Label(root, text=i).pack()
 
 
 def OpenFile():
@@ -56,43 +99,50 @@ def ReadFile():
         if file and len(file) > 1:
             raise errors.UnsupportedFotmatError('Cannot Read More than 1 File')
         else:
-            with open(file[0], 'r') as f:
-                label = Label(root, text=f.read()).pack()
+            popup(file, 'Read')
+    except NameError as err:
+        logger.error(err)
+        messagebox.showerror(title="NotDefined",
+                             message="File Not Selected!")
     except Exception as err:
-        logger.exception(err)
+        logging.exception(err)
 
 
 def write_to_file():
-    text = data.get()
+    text = write_data.get()
     try:
         if file and len(file) > 1:
             raise errors.UnsupportedFotmatError('Cannot Read More than 1 File')
         else:
             if os.path.getsize(file[0]) > 0:
                 with open(file[0], 'a') as f:
+                    f.write('\n')
                     f.write(text)
                 logger.debug(
                     'Writing Successfull into the File!! [APPEND MODE]')
+                messagebox.showinfo(
+                    title='Saved', message='Data Appended to the File')
             else:
                 with open(file[0], 'w') as f:
                     f.write(text)
                 logger.debug(
                     'Writing Successfull into the File!! [OVERWRITE MODE]')
+                messagebox.showinfo(
+                    title='Saved', message='Data Overwritten to the File')
     except Exception as err:
-        logger.exception(err)
+        logger.error(f"write_to_file - {err}")
+        messagebox.showerror(title="WriteToFileError",
+                             message=err)
 
 
 def WriteFile():
-    global data
     try:
         if file:
-            data = Entry(root, width=30)
-            data.pack()
-            btn = Button(root, text='WriteToFile',
-                         command=write_to_file).pack()
+            popup(file, mode='Write')
     except NameError as err:
-        logger.error(err)
-        label = Label(root, text='Neet to Select a File First!!').pack()
+        logger.error(f"WriteFile - {err}")
+        messagebox.showerror(title="NotDefined",
+                             message="File Not Selected!")
 
 
 def OpenImg():
@@ -107,6 +157,20 @@ def OpenImg():
         ViewImage()
 
 
+def ListContents():
+    global in_dir_contents
+    try:
+        in_dir_contents = os.listdir(dirname)
+        contens = []
+        for i in in_dir_contents:
+            contens.append(i)
+        popup(contens, 'List')
+    except Exception as err:
+        logger.error(f"ListContents - {err}")
+        messagebox.showerror(title="NotDefined",
+                             message="Directory Not Selected!")
+
+
 def ViewImage():
     global img
     new_window = Toplevel()
@@ -118,21 +182,169 @@ def ViewImage():
                  command=new_window.destroy).pack()
 
 
-btn_dir = Button(root, text='OpenDirectory', command=OpenDirectory,
-                 padx=100, pady=50).grid(row=0, column=0)  # Done - Alignment
+def RenameFile():
+    try:
+        if file:
+            popup(file, mode='RenameFile')
+    except NameError as err:
+        logger.error(f"RenameFile - {err}")
+        messagebox.showerror(title="NotDefined",
+                             message="File Not Selected!")
 
-btn_file = Button(root, text="OpenFile", command=OpenFile,
-                  padx=100, pady=50).grid(row=0, column=1)  # Done - Alignment
 
-btn_write = Button(root, text='WriteFile', command=WriteFile,
-                   padx=115, pady=50).grid(row=1, column=0)  # Done - Alignment
+def file_rename():
+    newFileName = newname.get() + '.txt'
+    if newFileName:
+        val = messagebox.askyesno(
+            title='Rename', message='Do You want to continue?')
+        if val:
+            shutil.copyfile(file[0], os.path.join(
+                os.path.split(file[0])[0], newFileName))
+            logger.info(f'Copying File Successfule to {newFileName}')
+            os.remove(file[0])
+            logger.warning(f'Successfully Removed {file[0]}')
+            window.destroy
+        else:
+            logger.critical(
+                f'Rename - operation Terminated By the {getpass.getuser()}')
+
+
+def RenameDirectory():
+    try:
+        if dirname:
+            popup(dirname, mode='RenameDir')
+    except NameError as err:
+        logger.error(f"RenameDirectory - {err}")
+        messagebox.showerror(title="NotDefined",
+                             message="Directory Not Selected!")
+
+
+def dir_rename():
+    newDirName = newdirname.get()
+    if newDirName:
+        val = messagebox.askyesno(
+            title='Rename', message='Do You want to continue?')
+        if val:
+            path = os.path.join(os.path.split(dirname)[0], newDirName)
+            shutil.copytree(dirname, path)
+            logger.info(f'Copying Directory Contents Successfule to {path}')
+            shutil.rmtree(dirname)
+            logger.warning(f'Successfully Removed {dirname}')
+            window.destroy
+        else:
+            logger.critical(
+                f'Rename - operation Terminated By the {getpass.getuser()}')
+
+
+def MakeDirectory():
+    try:
+        if dirname:
+            popup(dirname, mode='MakeDir')
+    except NameError as err:
+        logger.error(f"MakeDirectory - {err}")
+        messagebox.showerror(title="NotDefined",
+                             message="Parent Directory Not Selected!")
+
+
+def make_dir():
+    newDirName = newdirname.get()
+    if newDirName:
+        val = messagebox.askyesno(
+            title='Rename', message='Do You want to continue?')
+        if val:
+            path = os.path.join(dirname, newDirName)
+            os.mkdir(path)
+            logger.debug(f'Successfully Created {path}')
+            window.destroy
+        else:
+            logger.critical(
+                f'Rename - operation Terminated By the {getpass.getuser()}')
+
+
+def DeleteFile():
+    try:
+        if file:
+            os.remove(file[0])
+            logger.warning(f'Successfully Removed {file[0]}')
+    except NameError as err:
+        logger.error(f"DeleteFile - {err}")
+        messagebox.showerror(title="NotDefined",
+                             message="File Not Selected!")
+
+
+def DeleteDirectory():
+    try:
+        if dirname:
+            if len(os.listdir(dirname)) == 0:
+                os.rmdir(dirname)
+            else:
+                shutil.rmtree(dirname)
+            logger.warning(f'Successfully Removed {dirname}')
+    except NameError as err:
+        logger.error(f"DeleteDirectory - {err}")
+        messagebox.showerror(title="NotDefined",
+                             message="Directory Not Selected!")
+
+
+def MoveFile():
+    pass
+
+
+def MoveDirectory():
+    pass
+
+
+def CopyFile():
+    pass
+
+
+def CopyDirectory():
+    pass
+
+
+btn_dir = Button(root, text='SelectDirectory', command=OpenDirectory,
+                 padx=50, pady=30).grid(row=0, column=0, pady=10)  # Works
+
+btn_file = Button(root, text="SelectFile", command=OpenFile,
+                  padx=50, pady=30).grid(row=0, column=1)  # Works
 
 btn_read = Button(root, text='ReadFile', command=ReadFile,
-                  padx=101, pady=50).grid(row=1, column=1)  # Done - Alignment
+                  padx=50, pady=30).grid(row=0, column=2)  # Works
+
+btn_write = Button(root, text='WriteFile', command=WriteFile,
+                   padx=50, pady=30).grid(row=1, column=0, pady=10)  # Works
 
 btn_img = Button(root, text='OpenImg', command=OpenImg,
-                 padx=112, pady=50).grid(row=2, column=0)  # Done - Alignment
+                 padx=50, pady=30).grid(row=1, column=1)  # Works
 
+btn_list = Button(root, text="ListFileDir", command=ListContents,
+                  padx=50, pady=30).grid(row=1, column=2)   # Works
+
+btn_rename_file = Button(root, text="RenameFile", command=RenameFile,
+                         padx=50, pady=30).grid(row=2, column=0, pady=10)  # Works
+
+btn_rename_dir = Button(root, text="RenameDir", command=RenameDirectory,
+                        padx=50, pady=30).grid(row=2, column=1)   # Works
+
+btn_mkdir = Button(root, text="MakeDir", command=MakeDirectory,
+                   padx=50, pady=30).grid(row=2, column=2)  # Works
+
+btn_delete_dir = Button(root, text="DelDir", command=DeleteDirectory,
+                        padx=50, pady=30).grid(row=3, column=1)  # Works
+
+btn_delete_file = Button(root, text="DelFile", command=DeleteFile,
+                         padx=50, pady=30).grid(row=3, column=0)  # Works
+
+btn_cut_copy = Button(root, text="MvFile", command=MoveFile,
+                      padx=50, pady=30).grid(row=3, column=2)
+
+btn_cut_copy = Button(root, text="MvDir", command=MoveDirectory,
+                      padx=50, pady=30).grid(row=4, column=0)
+
+btn_cut_copy = Button(root, text="CpFile", command=CopyFile,
+                      padx=50, pady=30).grid(row=4, column=1)
+
+btn_cut_copy = Button(root, text="CpDir", command=CopyDirectory,
+                      padx=50, pady=30).grid(row=4, column=2)
 if __name__ == '__main__':
     root.mainloop()
-    # TODO Fix Alingmment
