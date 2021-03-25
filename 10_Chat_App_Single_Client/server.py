37b59652ffffbc20c31ee906be50f3832ea43eeb
Subhadeep Banerjee <subhadeep762@gmail.com>
2021-03-26 01:13:30 +0530

    Add onesided Message Feature

--- 10_Chat_App_Single_Client/server.py
+++ 10_Chat_App_Single_Client/server.py
@@ -1,3 +1,4 @@
+import time
 import socket
 import logging
 import threading
@@ -8,19 +9,33 @@ PORT = 9999
 ADDR = (HOST, PORT)
 HEADER = 64
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = '!DISCONNECT'
+DISCONNECT_MESSAGE = '!quit'
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     format='%(asctime)s  |  %(name)s  |  %(message)s')
 logger.setLevel(10)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+
+def createSocket():
+    global server
+    try:
+        logger.warning("[ESTABLISHING CONNECTION] - Creating Network Socket")
+        time.sleep(2)
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(ADDR)
+        logger.info(f"[PORT BINDING] - COMPLETE - {ADDR}")
+        time.sleep(1)
+        server.listen()
+        logger.debug(f"[LISTENING] on port - {PORT}")
+        time.sleep(1.5)
+    except socket.error as err:
+        logger.error(f"[SOCKET CREATION ERROR] - {err}")
 
 
 def handleClient(conn, addr):
     logger.info(f"[NEW CONNECTION] - {addr} CONNECTED")
+    time.sleep(2)
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
@@ -34,12 +49,12 @@ def handleClient(conn, addr):
 
 
 def start():
-    server.listen()
-    logger.debug(f"[LISTENING] Server is listening on {HOST}")
+    createSocket()
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handleClient, args=(conn, addr))
         thread.start()
+        time.sleep(1)
         logger.info(f"[ACTIVE CONNECTION] {threading.active_count() - 1}")
 
 
