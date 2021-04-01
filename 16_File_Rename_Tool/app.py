749f4453df0406cc74089b09f8b4b2cb69af2954
Subhadeep Banerjee <subhadeep762@gmail.com>
2021-04-01 17:56:50 +0530

    File Rename Tool

--- 16_File_Rename_Tool/app.py
+++ 16_File_Rename_Tool/app.py
@@ -1,13 +1,19 @@
 import os
 import sys
+import errno
 import shutil
 
 
 def main(orgFile, newName):
     path = os.path.split(orgFile)[0]
-    a = shutil.copyfile(orgFile, os.path.join(path, newName))
-    os.remove(orgFile)
-    print("Operation Complete, You may check now.")
+    try:
+        a = shutil.copyfile(orgFile, os.path.join(path, newName))
+        os.remove(orgFile)
+    except FileNotFoundError as err:
+        if err.errno == errno.ENOENT:
+            print('\nThe File Does Not Exist.')
+    else:
+        print("Operation Complete, You may check now.")
 
 
 if __name__ == '__main__':
